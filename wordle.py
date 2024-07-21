a20e2b784171d587e3577aa284cf22d164889b7d
98arvin <arvindoriani@gmail.com>
2024-07-21 22:58:30 +0200

    first commit

---
+++ wordle.py
@@ -0,0 +1,28 @@
+import random
+
+def wordle():
+    random_words = ["snake", "slave", "catch", "match", "binge", "blown", "black", "music", "dance", "query", "funny", "slash", "names", "users", "parts", "paths", "plank", "clark", "power", "flown", "clown", "shows", "shown", "sword", "grass", "grape", "petty", "lower", "sweet"]
+    counter = 0
+    chosen_word = random.choice(random_words)
+    guessed = []
+
+    print(f"The chosen word: {chosen_word}")
+
+    while counter < 7:
+        guess = input(f"Guess: ")
+        for char in guess:
+            if char in chosen_word:
+                if char not in guessed:
+                    guessed.append(char)
+        joined = "".join(sorted(guessed))
+        answer = "".join(sorted(chosen_word))
+        print(f"GUESSED: {joined}")
+        print(f"ANSWER: {chosen_word}")
+        
+        if joined == answer:
+            return print("WINNER!!!")
+        else:
+            counter += 1
+    return print("LOSERRRRRRRRRR!")
+
+wordle()

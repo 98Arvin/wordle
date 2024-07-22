6fff7251da0d4fd2f266c31e0fd11b9f0a750393
98Arvin <arvindoriani@gmail.com>
2024-07-22 13:51:58 +0200

    Added timer

--- wordle.py
+++ wordle.py
@@ -1,15 +1,21 @@
-import random
+import random, time
 
 def wordle():
     random_words = ["snake", "slave", "catch", "match", "binge", "blown", "black", "music", "dance", "query", "funny", "slash", "names", "users", "parts", "paths", "plank", "clark", "power", "flown", "clown", "shows", "shown", "sword", "grass", "grape", "petty", "lower", "sweet"]
-    counter = 0
+    counter = 1
     chosen_word = random.choice(random_words)
     guessed = []
+    start_time = time.time()
 
     # print(f"The chosen word: {chosen_word}")
 
-    while counter < 7:
-        while len(guess := input(f"Guess: ")) != 5:
+    print("=>= =================CONTAINDLE ================== =<=")
+    print("=>= YOUR STINT MUST CONTAIN ALL THE LETTERS TO WIN =<=")
+    print()
+
+    while counter < 11:
+        
+        while len(guess := input(f"→ ")) != 5:
             print("Has to be 5 chars")
         for char in guess:
             if char in chosen_word:
@@ -17,13 +23,15 @@ def wordle():
                     guessed.append(char)
         joined = "".join(sorted(guessed))
         answer = "".join(sorted(set(chosen_word)))
-        print(f"GUESSED: {joined}")
+        print(f"GUESS {counter}/10 → {joined.upper()}")
         # print(f"ANSWER: {answer}")
         
         if joined == answer:
-            return print("WINNER!!!")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            return print(f">{chosen_word.upper()}< IS INDEED THE CORRECT WORD || YOU HAD {10 - counter} GUESS(ES) REMAINING || TIME USED: {elapsed_time:.2f} SECONDS")
         else:
             counter += 1
-    return print(f"WRONG! The answer is {chosen_word}")
+    return print(f"WRONG! The answer is {chosen_word.upper()}")
 
 wordle()
